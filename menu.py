594a1a21322bd61172c77aa8c5feb279455376ed
Sage Elliott <wildtroll@gmail.com>
2015-08-18 15:42:17 -0700

    feat: add login button to login.html
    - create templates/login.html
    - add html elements
    - add ajax and google api scripts
    - create sign in button in dighInButton div

--- menu.py
+++ menu.py
@@ -22,7 +22,7 @@ session = DBSession()
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is {}".format(login_session['state'])
+    return render_template('login.html')
 
 
 # Make API Endpoint for full list of Restaurants(GET request)
