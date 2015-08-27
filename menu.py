5b1bfcaa70850cee3257cde8ee8d4d700b732cbb
Sage Elliott <wildtroll@gmail.com>
2015-08-26 21:36:05 -0700

    add createUser method to menu.py to get info from login session and pass it to the database

--- menu.py
+++ menu.py
@@ -303,6 +303,16 @@ def deleteMenuItem(restaurant_id, menu_id):
         return render_template('deletemenuitem.html', restaurant=restaurant, item = deleteItem)
 
 
+def createUser(login_session):
+    newUser = User(name = login_session['username'],
+                    email = login_session['email'],
+                    picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session['email']).one()
+    return user
+
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
