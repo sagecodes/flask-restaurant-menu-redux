156dec0e284e914e69ae30487512c576908ebd9e
Sage Elliott <wildtroll@gmail.com>
2015-08-27 13:21:02 -0700

    in gconnect add check to see if user exisits, n=if not create new one

--- menu.py
+++ menu.py
@@ -108,6 +108,13 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
+    # check if user exists, if not create new user:
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
